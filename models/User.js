de126bd200ed667980d117c64232ae443eec0ce8
Dhaval khunt <dhavalkhunt17@gmail.com>
2025-02-13 11:31:18 -0500

    login fixed

--- models/User.js
+++ models/User.js
@@ -37,16 +37,11 @@ const userSchema = new Schema({
   }]
 }, { timestamps: true });
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
+
 
 // Compare password
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);  
