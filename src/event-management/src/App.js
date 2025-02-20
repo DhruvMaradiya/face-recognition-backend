8d76c9add93c4f542676c3ffd6ab420e028ac58b
dhruv <dhruvmvr@gmail.com>
2025-02-20 13:59:57 -0500

    working apis + frontend

---
+++ src/event-management/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EventForm from "./components/EventForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Create Event</h1>
+      <EventForm />
+    </div>
+  );
+}
+
+export default App;
