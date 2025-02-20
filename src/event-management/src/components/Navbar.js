3f7ed60d93d911c6f3982a4911fe7163d25d8a9e
dhruv <dhruvmvr@gmail.com>
2025-02-20 18:30:23 -0500

    deleted useless files

--- src/event-management/src/components/Navbar.js
+++ src/event-management/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="bg-blue-600 p-4 flex justify-between text-white">
-            <h1 className="text-xl font-bold">Event Manager</h1>
+            <h1 className="text-xl font-bold"><Link to="/">Event Manager</Link></h1>
             <Link to="/create" className="bg-white text-blue-600 px-4 py-2 rounded-md">
                 + Create Event
             </Link>
